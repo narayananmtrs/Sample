c336c37aed3a28f68fe04e3727f442365de30922
narayananmtrs <73398262+narayananmtrs@users.noreply.github.com>
2020-10-24 22:42:45 +0530

    Create Odd.java

---
+++ Odd.java
@@ -0,0 +1,15 @@
+public class Odd{
+
+  public static void main(string[] args){
+    
+    int i = 10;
+    
+    if(i % 10 == 0){
+      System.out.println("Even");
+    }else{
+      System.out.println("Odd");
+    }
+    
+   }
+   
+  } 
